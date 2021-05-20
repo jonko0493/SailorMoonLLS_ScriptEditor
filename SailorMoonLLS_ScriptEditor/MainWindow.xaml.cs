5123041bafb03f5587f7a344e504fb0cd778677a
jonko0493 <jonko0493@protonmail.com>
2021-05-19 22:11:02 -0700

    Initial commit

---
+++ SailorMoonLLS_ScriptEditor/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SailorMoonLLS_ScriptEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public NutrRaw Nutr { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FileMenuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Decompressed NUTR file|*.nutr.decompressed"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Nutr = NutrRaw.ParseFromFile(openFileDialog.FileName);
+                commandsListBox.ItemsSource = Nutr.PostScriptCommands.Select(c => $"{c.Line(Nutr.Script.Select(n => n.Text).ToList())} (0x{c.LineNumberMaybe:X2}):\t\t" +
+                    $"0x{string.Join(" ", c.CommandBytes.Select(cb => $"{cb:X2}"))}");
+            }
+        }
+
+        private void FileMenuSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Decompressed NUTR file|*.nutr.decompressed"
+            };
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                Nutr.WriteToFile(saveFileDialog.FileName);
+            }
+        }
+
+        private void FileMenuExtractScript_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Text File|*.txt"
+            };
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                var scriptText = Nutr.PostScriptCommands.Select(c => c.Line(Nutr.Script.Select(n => n.Text).ToList()));
+                List<string> extractedScript = new List<string>();
+
+                bool message = false;
+                foreach (string line in scriptText)
+                {
+                    if (message)
+                    {
+                        extractedScript.Add(line);
+                        message = false;
+                    }
+                    else if (line == "msg")
+                    {
+                        message = true;
+                    }
+                    else if (line == "talk")
+                    {
+                        extractedScript.Add("\n");
+                    }
+                }
+
+                File.WriteAllLines(saveFileDialog.FileName, extractedScript.ToArray());
+            }
+        }
+    }
+}
---
+++ SailorMoonLLS_ScriptEditor/NutrRaw.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SailorMoonLLS_ScriptEditor
+{
+    public class NutrRaw
+    {
+        public byte[] BoilerPlate { get; private set; }
+        public List<NutrRawLine> Script { get; set; } = new List<NutrRawLine>();
+        public List<byte> PostScript { get; set; } = new List<byte>();
+        public List<PostScriptCommand> PostScriptCommands { get; set; } = new List<PostScriptCommand>();
+        public List<byte> PostPostScript { get; set; } = new List<byte>();
+
+        public static int BoilerPlateLength = 0x1A3;
+
+        public static NutrRaw ParseFromFile(string file)
+        {
+            byte[] data = File.ReadAllBytes(file);
+            NutrRaw nutrRaw = new NutrRaw();
+            int filePtr;
+
+            nutrRaw.BoilerPlate = data.Take(BoilerPlateLength).ToArray();
+            for (filePtr = BoilerPlateLength; filePtr < data.Length;)
+            {
+                int length = BitConverter.ToInt32(new byte[] { data[filePtr], data[filePtr + 1], data[filePtr + 2], data[filePtr + 3] });
+                filePtr += 4;
+
+                byte[] textBytes = new byte[length];
+                for (int j = 0; j < length; j++)
+                {
+                    textBytes[j] = data[filePtr];
+                    filePtr++;
+                }
+                string text = Encoding.UTF8.GetString(textBytes);
+
+                if (text == "suspend")
+                {
+                    nutrRaw.Script.Add(new NutrRawLine { Text = text });
+                    break;
+                }
+
+                byte[] lineEnd = new byte[] { data[filePtr], data[filePtr + 1], data[filePtr + 2], data[filePtr + 3] };
+                if (!lineEnd.SequenceEqual(NutrRawLine.LineEnd))
+                {
+                    throw new FileFormatException($"Expected line end after string '{text}' but found sequence '0x{lineEnd[0]:X2} 0x{lineEnd[1]:X2} 0x{lineEnd[2]:X2} 0x{lineEnd[3]:X2}' instead.");
+                }
+                filePtr += 4;
+
+                nutrRaw.Script.Add(new NutrRawLine { Text = text });
+            }
+
+            bool encounteredTrapTrap = false;
+            for (; ; filePtr++)
+            {
+                if (data[filePtr - 8] == 'T' && data[filePtr - 7] == 'R' && data[filePtr - 6] == 'A' && data[filePtr - 5] == 'P' &&
+                    data[filePtr - 4] == 'T' && data[filePtr - 3] == 'R' && data[filePtr - 2] == 'A' && data[filePtr - 1] == 'P')
+                {
+                    if (encounteredTrapTrap)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        encounteredTrapTrap = true;
+                    }
+                }
+
+                nutrRaw.PostScript.Add(data[filePtr]);
+            }
+
+            for (; !(data[filePtr] == 'T' && data[filePtr + 1] == 'R' && data[filePtr + 2] == 'A' && data[filePtr + 3] == 'P');)
+            {
+                int lineNumber = BitConverter.ToInt32(new byte[] { data[filePtr], data[filePtr + 1], data[filePtr + 2], data[filePtr + 3] });
+                if (lineNumber > nutrRaw.Script.Count)
+                {
+                    break;
+                }
+
+                nutrRaw.PostScriptCommands.Add(new PostScriptCommand
+                {
+                    LineNumberMaybe = lineNumber,
+                    CommandBytes = new byte[] { data[filePtr + 4], data[filePtr + 5], data[filePtr + 6], data[filePtr + 7] },
+                });
+                filePtr += 8;
+            }
+
+            for (; filePtr < data.Length; filePtr++)
+            {
+                nutrRaw.PostPostScript.Add(data[filePtr]);
+            }
+
+            return nutrRaw;
+        }
+
+        public void WriteToFile(string file)
+        {
+            List<byte> data = new List<byte>();
+            data.AddRange(BoilerPlate);
+            foreach (NutrRawLine line in Script)
+            {
+                data.AddRange(line.ToBytes());
+            }
+            data.AddRange(PostScript);
+            foreach (PostScriptCommand command in PostScriptCommands)
+            {
+                data.AddRange(command.ToBytes());
+            }
+            data.AddRange(PostPostScript);
+
+            File.WriteAllBytes(file, data.ToArray());
+        }
+    }
+
+    public class NutrRawLine
+    {
+        public int Length => Encoding.UTF8.GetByteCount(Text);
+        public string Text { get; set; }
+        public static byte[] LineEnd => new byte[] { 0x10, 0x00, 0x00, 0x08 };
+
+
+        public byte[] ToBytes()
+        {
+            List<byte> bytes = new List<byte>();
+            bytes.AddRange(BitConverter.GetBytes(Length));
+            bytes.AddRange(Encoding.UTF8.GetBytes(Text));
+            if (Text != "suspend")
+            {
+                bytes.AddRange(LineEnd);
+            }
+            
+            return bytes.ToArray();
+        }
+    }
+
+    public class PostScriptCommand
+    {
+        public int LineNumberMaybe { get; set; }
+        public byte[] CommandBytes { get; set; }
+
+        public string Line(List<string> lines)
+        {
+            return lines[LineNumberMaybe];
+        }
+
+        public byte[] ToBytes()
+        {
+            List<byte> bytes = new List<byte>();
+            bytes.AddRange(BitConverter.GetBytes(LineNumberMaybe));
+            bytes.AddRange(CommandBytes);
+
+            return bytes.ToArray();
+        }
+    }
+}
